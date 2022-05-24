b2f955b51c81353e9c2527c3405ad111c882d351
DobJa <jandobrosolski@gmail.com>
2022-05-24 19:32:21 +0200

    tweaked label assignement, loss evaluation working now

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ base_model.trainable = False
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 disc_layer1 = tf.keras.layers.Dense(1024, activation = "relu")
 disc_layer2 = tf.keras.layers.Dense(512, activation = "relu")
-prediction_layer = tf.keras.layers.Dense(43, activation = "softmax")
+prediction_layer = tf.keras.layers.Dense(400, activation = "softmax")
 
 
 model = tf.keras.Sequential([
@@ -36,27 +36,32 @@ model = tf.keras.Sequential([
 
 
 labels = {}
-
-model.compile(optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0001),
-              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-              metrics=['accuracy'])
 index = 0
 for cat in os.listdir(TRAINDATADIR):
     labels[index] = cat
     index+=1
 
+model.compile(optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.0001),
+              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['accuracy'])
+
 index = 0
 for cat in os.listdir(TRAINDATADIR):
     cat_path = os.path.join(TRAINDATADIR, str(cat))
-    index += 1
     for img in os.listdir(cat_path):
         entry = cv2.imread(cat_path + '/' + str(img))
         entry = entry / 255.0
         entry = cv2.resize(entry, (80,80), interpolation=cv2.INTER_NEAREST)
         data_list.append(entry)
         label_list.append(index)
+    index += 1
 x_train= np.array(data_list)
 y_train = np.array(label_list)
+
+#print(x_train.shape)
+#print("\n")
+#print(y_train.shape)
+#print("\n")
 
 data_list.clear()
 label_list.clear()
@@ -65,17 +70,22 @@ label_list.clear()
 index = 0
 for cat in os.listdir(VALDATADIR):
     cat_path = os.path.join(VALDATADIR, str(cat))
-    index += 1
     for img in os.listdir(cat_path):
         entry = cv2.imread(cat_path + '/' + str(img))
         entry = entry / 255.0
         entry = cv2.resize(entry, (80,80), interpolation=cv2.INTER_NEAREST)
         data_list.append(entry)
         label_list.append(index)
+    index += 1
 x_validate = np.array(data_list)
 y_validate = np.array(label_list)
 
+#print(x_validate.shape)
+#print("\n")
+#print(y_validate.shape)
+#print("\n")
 data_list.clear()
+
 label_list.clear()
 
 
@@ -83,15 +93,20 @@ label_list.clear()
 index = 0
 for cat in os.listdir(TESTDATADIR):
     cat_path = os.path.join(TESTDATADIR, str(cat))
-    index += 1
     for img in os.listdir(cat_path):
         entry = cv2.imread(cat_path + '/' + str(img))
         entry = entry / 255.0
         entry = cv2.resize(entry, (80,80), interpolation=cv2.INTER_NEAREST)
         data_list.append(entry)
         label_list.append(index)
+    index += 1
 x_test = np.array(data_list)
 y_test = np.array(label_list)
+
+#print(x_test.shape)
+#print("\n")
+#print(y_test.shape)
+#print("\n")
 
 index = 0
 data_list.clear()
@@ -99,12 +114,12 @@ label_list.clear()
 
 
 history = model.fit(x_train,y_train, batch_size = 32,
-                    epochs = 15,
+                    epochs = 5,
                     shuffle=True,
                     validation_data = (x_validate, y_validate))
 
 print(f"\n SPLIT1 TESTING \n")
 
 results = model.evaluate(x_test, y_test, batch_size = 32)
-model.save_weights('MODEL1/')
+#model.save_weights('MODEL1/')
 
